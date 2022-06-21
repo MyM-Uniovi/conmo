b03a0100d718d2023dc734b02c733b9659f377bc
LucasP <uo264808@uniovi.es>
2022-06-21 10:34:48 +0200

    Pre-release setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name ='conmo',
-    version = '1.0.0',
+    version = '1.0.1',
     author = 'GrupoMyM',
     author_email = 'mym.inv.uniovi@gmail.com',
     url ='https://github.com/MyM-Uniovi/conmo',
